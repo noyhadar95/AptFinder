d534303e9b763c70ec5e3ed1d1fb69affc95cb20
Noy Hadar <noyhadar95@gmail.com>
2023-04-25 01:06:51 +0300

    Remove old tries

--- AptFinder.py
+++ AptFinder.py
@@ -1,4 +1,3 @@
-from FacebookScraper import FacebookScraper
 from facebook_scraper import get_posts
 
 ### Facebook user: Boris Red
@@ -8,23 +7,16 @@ group_id = "184920528370332"
 
 
 def run():
-    # older_posts = get_posts('184920528370332', pages=1, timeout=30, credentials=(email, password), encoding='utf-8')
-    # with open('posts.txt', 'w', encoding="utf-8") as f:
-    #     for p in older_posts:
-    #         f.write(p['text'])
-    #         f.write('\n\n')
+    older_posts = get_posts('184920528370332', pages=1, timeout=30, credentials=(email, password), encoding='utf-8')
+    with open('posts.txt', 'w', encoding="utf-8") as f:
+        for p in older_posts:
+            f.write(p['text'])
+            f.write('\n\n')
 
-    # facebook_scraper = FacebookScraper(email, password, [group_id])
-    # with FacebookScraper(email, password, [group_id]) as fs:
-    #     older_posts = fs.get_older_posts()
-    #     with open('posts.txt', 'w', encoding="utf-8") as f:
-    #         for p in older_posts:
-    #             f.write(p.text)
-    #             f.write('\n\n')        
-        # new_post = facebook_scraper.run()
-        # parsed_post = PostManager.parse(new_post)
-        # if PostManager.filter(parsed_post):
-        #     telegram_bot.send(parsed_post)
+    # new_post = facebook_scraper.run()
+    # parsed_post = PostManager.parse(new_post)
+    # if PostManager.filter(parsed_post):
+    #     telegram_bot.send(parsed_post)
 
 
 if __name__ == '__main__':
